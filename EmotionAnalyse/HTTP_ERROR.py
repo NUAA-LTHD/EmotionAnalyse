61553c08f8bb4651d04f1f8a598d03a86fdc9631
李 亚宁 <1553770945@qq.com>
2020-03-21 15:32:45 +0800

    修复了一些小BUG，加入了404页面的提示。

--- EmotionAnalyse/HTTP_ERROR.py
+++ EmotionAnalyse/HTTP_ERROR.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 def not_found(request,exception):
     context=dict()
-    context['message']="页面迷失在海洋中..."
+    context['err_msg']="页面迷失在海洋中..."
     return render(request,"404.html",context)
--- EmotionAnalyse/settings.py
+++ EmotionAnalyse/settings.py
@@ -137,7 +137,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_new')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR,'login/static'),
 ]
 
 EMAIL_USE_SSL = True
